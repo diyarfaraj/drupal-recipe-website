eb25e2e6bf8ed8fd712dd454327aeb2f146cf253
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-06-22 15:45:49 +0200

    initial commit

---
+++ sites/default/files/php/twig/5d0e0839d8de2_node.html.twig_D2WjheB5TA4AgM1RpuFMblDe8/_tosTaPzy0tfQvNv6fLZKUoffwjmYqInS2viWbc6BOM.php
@@ -0,0 +1,177 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/mayo/templates/node.html.twig */
+class __TwigTemplate_a68b3f99ce4d6b596e5fd6256f850f62a5babc77eff0b3e0180f022550fcff57 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 65, "if" => 78, "trans" => 88];
+        $filters = ["clean_class" => 67, "escape" => 75, "without" => 95];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'trans'],
+                ['clean_class', 'escape', 'without'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 65
+        $context["classes"] = [0 => "node", 1 => ("node--type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed($this->getAttribute(        // line 67
+($context["node"] ?? null), "bundle", [])))), 2 => (($this->getAttribute(        // line 68
+($context["node"] ?? null), "isPromoted", [], "method")) ? ("node--promoted") : ("")), 3 => (($this->getAttribute(        // line 69
+($context["node"] ?? null), "isSticky", [], "method")) ? ("node--sticky") : ("")), 4 => (( !$this->getAttribute(        // line 70
+($context["node"] ?? null), "isPublished", [], "method")) ? ("node--unpublished") : ("")), 5 => ((        // line 71
+($context["view_mode"] ?? null)) ? (("node--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null))))) : ("")), 6 => "clearfix"];
+        // line 75
+        echo "<article";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+  <header>
+    ";
+        // line 77
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null)), "html", null, true);
+        echo "
+    ";
+        // line 78
+        if ( !($context["page"] ?? null)) {
+            // line 79
+            echo "      <h2";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["title_attributes"] ?? null), "addClass", [0 => "node__title"], "method")), "html", null, true);
+            echo ">
+        <a href=\"";
+            // line 80
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null)), "html", null, true);
+            echo "\" rel=\"bookmark\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null)), "html", null, true);
+            echo "</a>
+      </h2>
+    ";
+        }
+        // line 83
+        echo "    ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null)), "html", null, true);
+        echo "
+    ";
+        // line 84
+        if (($context["display_submitted"] ?? null)) {
+            // line 85
+            echo "      <div class=\"node__meta\">
+        ";
+            // line 86
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_picture"] ?? null)), "html", null, true);
+            echo "
+        <span";
+            // line 87
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_attributes"] ?? null)), "html", null, true);
+            echo ">
+          ";
+            // line 88
+            echo t("Submitted by @author_name on @date", array("@author_name" => ($context["author_name"] ?? null), "@date" => ($context["date"] ?? null), ));
+            // line 89
+            echo "        </span>
+        ";
+            // line 90
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["metadata"] ?? null)), "html", null, true);
+            echo "
+      </div>
+    ";
+        }
+        // line 93
+        echo "  </header>
+  <div";
+        // line 94
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content_attributes"] ?? null), "addClass", [0 => "node__content", 1 => "clearfix"], "method")), "html", null, true);
+        echo ">
+    ";
+        // line 95
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->withoutFilter($this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "comment", "links"), "html", null, true);
+        echo "
+  </div>
+
+  ";
+        // line 98
+        if ($this->getAttribute(($context["content"] ?? null), "links", [])) {
+            // line 99
+            echo "    <div class=\"node__links\">
+      ";
+            // line 100
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "links", [])), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 103
+        echo "
+  ";
+        // line 104
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "comment", [])), "html", null, true);
+        echo "
+
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/mayo/templates/node.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  143 => 104,  140 => 103,  134 => 100,  131 => 99,  129 => 98,  123 => 95,  119 => 94,  116 => 93,  110 => 90,  107 => 89,  105 => 88,  101 => 87,  97 => 86,  94 => 85,  92 => 84,  87 => 83,  79 => 80,  74 => 79,  72 => 78,  68 => 77,  62 => 75,  60 => 71,  59 => 70,  58 => 69,  57 => 68,  56 => 67,  55 => 65,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/mayo/templates/node.html.twig", "C:\\xampp\\htdocs\\drupal1\\themes\\mayo\\templates\\node.html.twig");
+    }
+}
